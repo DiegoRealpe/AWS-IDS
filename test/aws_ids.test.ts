36388eab39ed1604f1ab4445631c56591101eafc
Diego <diegorealpet@gmail.com>
2024-04-13 21:21:52 -0500

    Initial commit

---
+++ test/aws_ids.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsIds from '../lib/aws_ids-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws_ids-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsIds.AwsIdsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
